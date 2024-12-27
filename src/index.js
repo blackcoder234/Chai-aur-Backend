bdec80b41a76f3fddaee8e1afdd0276d29294859
shadows237 <165073025+blackcoder234@users.noreply.github.com>
2024-12-27 19:45:02 +0530

    #changes are made

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import dotenv from 'dotenv'
-import connectDB from "./db/database.js";
-
+// const dotenv = require("dotenv");
+/*import dotenv from 'dotenv'
 dotenv.config({
     path:"./env"
-})
+})     // if this approach is encountering any issue  add this to the package.json in the srcipt option --> "dev": "nodemon -r dotenv/config --experimental-json-modules   src/index.js"*/
+
+import 'dotenv/config' // this is modern approach and there should not be any issue for this updated node.js
+import connectDB from "./db/database.js";
+
 
 connectDB()
