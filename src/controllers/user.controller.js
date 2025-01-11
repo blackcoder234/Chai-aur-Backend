9e1477de0b1537e15dbab99756ceb07a13ffa3cc
shadows237 <165073025+blackcoder234@users.noreply.github.com>
2025-01-11 09:38:29 +0530

    Add getUserChannelProfile function to retrieve user channel details with subscription info

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -315,7 +315,78 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
 })
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing")
+    }
+    //here we use the famous topic that is MongoDb Aggregation Pipeline ------ go to https://www.mongodb.com/docs/manual/core/aggregation-pipeline/
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLOwerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user ? _id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
 
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+    console.log(channel)
+
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel does not exits")
+    }
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, channel[0], "user channel fetched successfully")
+        )
+
+
+})
 
 
 // export default registerUser
@@ -328,5 +399,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
