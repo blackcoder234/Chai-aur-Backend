d94b7a582eb6703d58baf25f3f69fa9034981331
shadows237 <165073025+blackcoder234@users.noreply.github.com>
2025-01-11 09:44:24 +0530

    Fix formatting in getUserChannelProfile function for improved readability

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -353,7 +353,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
                 },
                 isSubscribed: {
                     $cond: {
-                        if: { $in: [req.user ? _id, "$subscribers.subscriber"] },
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
                         then: true,
                         else: false
                     }
